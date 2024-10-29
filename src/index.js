0e33a02684bb8f5aeec2168fa6da1c68c831c8b5
unknown <svorovali@mail.ru>
2024-10-29 15:06:08 +0300

    Init commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css'; // Импортируйте CSS файл, если есть
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root  = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
         <App />
